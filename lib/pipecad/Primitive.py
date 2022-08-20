14bb46936d39e73f595e68168ea54aeeef0fd85f
eryar <eryar@163.com>
2022-08-20 16:17:37 +0800

    Create Negative Dish #95

--- lib/pipecad/Primitive.py
+++ lib/pipecad/Primitive.py
@@ -688,12 +688,14 @@ def CreateCone():
 class DishDialog(QDialog):
     def __init__(self, parent = None):
         QDialog.__init__(self, parent)
+
+        self.dishItem = None
         
         self.setupUi()
     # __init__
 
     def setupUi(self):
-        self.setWindowTitle(self.tr("Create Dish"))
+        self.setWindowTitle(QT_TRANSLATE_NOOP("Design", "Create Dish"))
 
         self.horizontalLayout = QHBoxLayout(self)
         self.verticalLayout = QVBoxLayout()
@@ -705,13 +707,14 @@ class DishDialog(QDialog):
         self.textName = QLineEdit()
         self.textName.setMinimumWidth(150)
 
-        self.comboName.addItem(self.tr("Create"))
-        self.comboName.addItem(self.tr("Modify"))
+        self.comboName.addItem(QT_TRANSLATE_NOOP("Design", "Create"))
+        self.comboName.addItem(QT_TRANSLATE_NOOP("Design", "Modify"))
+        self.comboName.activated.connect(self.activateName)
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.comboName)
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.textName)
 
-        self.labelNegative = QLabel(self.tr("Negative"))
+        self.labelNegative = QLabel(QT_TRANSLATE_NOOP("Design", "Negative"))
         self.checkNegative = QCheckBox()
 
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.labelNegative)
@@ -728,19 +731,19 @@ class DishDialog(QDialog):
         # Dimensions.
         self.formLayout = QFormLayout()
 
-        self.labelDiameter = QLabel(self.tr("Diameter"))
+        self.labelDiameter = QLabel(QT_TRANSLATE_NOOP("Design", "Diameter"))
         self.textDiameter = QLineEdit("0.0")
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.labelDiameter)
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.textDiameter)
         
-        self.labelRadius = QLabel(self.tr("Radius"))
+        self.labelRadius = QLabel(QT_TRANSLATE_NOOP("Design", "Radius"))
         self.textRadius = QLineEdit("0.0")
 
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.labelRadius)
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.textRadius)
         
-        self.labelHeight = QLabel(self.tr("Height"))
+        self.labelHeight = QLabel(QT_TRANSLATE_NOOP("Design", "Height"))
         self.textHeight = QLineEdit("0.0")
 
         self.formLayout.setWidget(2, QFormLayout.LabelRole, self.labelHeight)
@@ -757,19 +760,19 @@ class DishDialog(QDialog):
         # Position.
         self.formLayout = QFormLayout()
 
-        self.labelPx = QLabel(self.tr("East"))
+        self.labelPx = QLabel(QT_TRANSLATE_NOOP("Design", "East"))
         self.textPx = QLineEdit("0.0")
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.labelPx)
         self.formLayout.setWidget(0, QFormLayout.FieldRole, self.textPx)
 
-        self.labelPy = QLabel(self.tr("North"))
+        self.labelPy = QLabel(QT_TRANSLATE_NOOP("Design", "North"))
         self.textPy = QLineEdit("0.0")
 
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.labelPy)
         self.formLayout.setWidget(1, QFormLayout.FieldRole, self.textPy)
 
-        self.labelPz = QLabel(self.tr("Up"))
+        self.labelPz = QLabel(QT_TRANSLATE_NOOP("Design", "Up"))
         self.textPz = QLineEdit("0.0")
 
         self.formLayout.setWidget(2, QFormLayout.LabelRole, self.labelPz)
@@ -800,7 +803,42 @@ class DishDialog(QDialog):
         self.horizontalLayout.addWidget(self.labelDiagram)
     # setupUi
 
-    def accept(self):
+    def activateName(self):
+        self.dishItem = None
+
+        aIndex = self.comboName.currentIndex
+        if aIndex == 1:
+            # modify dish
+            self.setWindowTitle(QT_TRANSLATE_NOOP("Design", "Modify Dish"))
+
+            aTreeItem = PipeCad.CurrentItem()
+            if aTreeItem.Type not in {"DISH", "NDIS"}:
+                QMessageBox.warning(self, "", QT_TRANSLATE_NOOP("Design", "Please select DISH/NDIS to modify!"))
+                return
+            # if
+
+            self.dishItem = aTreeItem
+
+            self.textName.setText(aTreeItem.Name)
+            self.checkNegative.setChecked(aTreeItem.Type == "NDIS")
+            self.checkNegative.setEnabled(False)
+
+            self.textDiameter.setText(str(aTreeItem.Diameter))
+            self.textRadius.setText(str(aTreeItem.Radius))
+            self.textHeight.setText(str(aTreeItem.Height))
+
+            aPosition = aTreeItem.Position
+            self.textPx.setText(str(aPosition.X))
+            self.textPy.setText(str(aPosition.Y))
+            self.textPz.setText(str(aPosition.Z))
+        else:
+            # create dish
+            self.setWindowTitle(QT_TRANSLATE_NOOP("Design", "Create Dish"))
+            self.checkNegative.setEnabled(True)
+        # if
+    # activateName
+
+    def createDish(self):
         aName = self.textName.text
 
         aPx = float(self.textPx.text)
@@ -810,7 +848,11 @@ class DishDialog(QDialog):
         try:
             PipeCad.StartTransaction("Create Dish")
 
-            PipeCad.CreateItem("DISH", aName)
+            if self.checkNegative.checked:
+                PipeCad.CreateItem("NDIS", aName)
+            else:
+                PipeCad.CreateItem("DISH", aName)
+            # if
 
             aTreeItem = PipeCad.CurrentItem()
             aTreeItem.Diameter = float(self.textDiameter.text)
@@ -823,6 +865,37 @@ class DishDialog(QDialog):
             QMessageBox.critical(self, "", e)
             raise e
         # try
+    # createDish
+
+    def modifyDish(self):
+        if self.dishItem is None:
+            return
+        # if
+
+        aName = self.textName.text
+
+        aPx = float(self.textPx.text)
+        aPy = float(self.textPy.text)
+        aPz = float(self.textPz.text)
+
+        PipeCad.StartTransaction("Modify Dish")
+
+        aTreeItem = self.dishItem
+        aTreeItem.Diameter = float(self.textDiameter.text)
+        aTreeItem.Radius = float(self.textRadius.text)
+        aTreeItem.Height = float(self.textHeight.text)
+        aTreeItem.Position = Position(aPx, aPy, aPz, aTreeItem.Owner)
+
+        PipeCad.CommitTransaction()
+    # modifyDish
+
+    def accept(self):
+        aIndex = self.comboName.currentIndex
+        if aIndex == 0:
+            self.createDish()
+        else:
+            self.modifyDish()
+        # if
 
         QDialog.accept(self)
     # accept
